cb09e376b509351ae0e8582db45ebf35e9986339
LuisCordova <luiscordova@michaelpage.pe>
2021-05-13 13:21:48 -0500

    improve unitTest using Moq library

--- Tranzact.WaterContainer.Test/OperatorTest.cs
+++ Tranzact.WaterContainer.Test/OperatorTest.cs
@@ -18,14 +18,7 @@ namespace Tranzact.WaterContainer.Test
         public void Setup()
         {
             this.waterContainerOperator = new Mock<IWaterContainerOperator>();
-        }
-
-        [Test]
-        public void return_error_if_dont_have_coordinates()
-        {
-            CoordinateDTO dto = new CoordinateDTO();
-            dto.CoordinateY = new int[] { 1 };
-            waterContainerOperator.Setup(p => p.ResultWaterContainer(It.IsAny<CoordinateDTO>())).Returns(() =>
+            this.waterContainerOperator.Setup(p => p.ResultWaterContainer(It.IsAny<CoordinateDTO>())).Returns((CoordinateDTO dto) =>
             {
                 var response = new WaterContainerResponse();
                 if (dto.CoordinateY.Length < 2)
@@ -50,11 +43,17 @@ namespace Tranzact.WaterContainer.Test
                     return response;
                 }
             });
+        }
 
+        [Test]
+        public void return_error_if_dont_have_coordinates()
+        {
+            CoordinateDTO request = new CoordinateDTO();
+            request.CoordinateY = new int[] { 1 };
             Assert.Throws<WaterContainerException>(
             () =>
             {
-                waterContainerOperator.Object.ResultWaterContainer(dto);
+                waterContainerOperator.Object.ResultWaterContainer(request);
             });
         }
 
@@ -62,35 +61,10 @@ namespace Tranzact.WaterContainer.Test
         [Test]
         public void return_correct_value_with_result_one()
         {
-            CoordinateDTO dto = new CoordinateDTO();
-            dto.CoordinateY = new int[] { 1, 1 };
-            waterContainerOperator.Setup(p => p.ResultWaterContainer(It.IsAny<CoordinateDTO>())).Returns(() =>
-            {
-                var response = new WaterContainerResponse();
-                if (dto.CoordinateY.Length < 2)
-                {
-                    throw new WaterContainerException("Coordinates not found");
-                }
-                else
-                {
-                    int lenght = dto.CoordinateY.Length;
-                    int output = 0;
-                    for (int i = 0; i < lenght; i++)
-                    {
-                        for (int j = lenght - 1; j > i; j--)
-                        {
-                            int baser = (j - i);
-                            int height = (dto.CoordinateY[i] < dto.CoordinateY[j]) ? dto.CoordinateY[i] : dto.CoordinateY[j];
-                            int area = baser * height;
-                            output = (area > output) ? area : output;
-                        }
-                    }
-                    response.Result = output;
-                    return response;
-                }
-            });
-            int result = waterContainerOperator.Object.ResultWaterContainer(dto).Result;
+            CoordinateDTO request = new CoordinateDTO();
+            request.CoordinateY = new int[] { 1, 1 };
             int expected = 1;
+            int result = waterContainerOperator.Object.ResultWaterContainer(request).Result;
             Assert.AreEqual(expected, result);
         }
     }
--- Tranzact.WaterContainer.Test/ServicesTest.cs
+++ Tranzact.WaterContainer.Test/ServicesTest.cs
@@ -17,41 +17,35 @@ namespace Tranzact.WaterContainer.Test
         public void Setup()
         {
             this.waterContainerServices = new Mock<IWaterContainerServices>();
+
+            this.waterContainerServices.Setup(p => p.getLength(It.IsAny<int[]>())).Returns((int[] len) => len.Length);
+
+            this.waterContainerServices.Setup(p => p.calculateArea(It.IsAny<int>(), It.IsAny<int>())).Returns((int baser, int height) => baser * height);
         }
 
-
         [Test]
-        public void get_lenght_cero() {
-            var len = new int[] {};
-            waterContainerServices.Setup(p => p.getLength(It.IsAny<int[]>())).Returns(() => {
-                return len.Length;
-            });
-            var result = waterContainerServices.Object.getLength(len);
+        public void get_lenght_in_array_without_values() {
+            var request = new int[] {};
             var expected = 0;
+            var result = waterContainerServices.Object.getLength(request);
             Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void get_lenght_in_array() {
-            var len = new int[] { 1, 2, 3, 6 };
-            waterContainerServices.Setup(p => p.getLength(It.IsAny<int[]>())).Returns(() => {
-                return len.Length;
-            });
-            var result = waterContainerServices.Object.getLength(len);
+        public void get_lenght_in_array_with_values() {
+            var request = new int[] { 1, 2, 3, 6 };
             var expected = 4;
+            var result = waterContainerServices.Object.getLength(request);
             Assert.AreEqual(expected, result);
         }
 
         [Test]
         public void get_base_by_height()
         {
-            var baser = 10;
-            var height = 20;
-            waterContainerServices.Setup(p => p.calculateArea(baser, height)).Returns(() => {
-                return baser * height;
-            });
-            var result = waterContainerServices.Object.calculateArea(baser, height);
+            var baseRequest = 10;
+            var heightRequest = 20;
             var expected = 200;
+            var result = waterContainerServices.Object.calculateArea(baseRequest, heightRequest);
             Assert.AreEqual(expected, result);
         }
 
